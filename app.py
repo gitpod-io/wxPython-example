71ceaa92822a2399614c54f7cb0434ec417aa5d1
Sean Hellum <seanhellum45@gmail.com>
2019-12-20 19:39:18 +0000

    update app

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import wx
 
-app = wx.App()
-frame = wx.Frame(parent=None, title='Hello World')
-frame.Show()
-app.MainLoop()
+
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title='Note Taking')
+        panel = wx.Panel(self)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.my_btn = wx.Button(panel, label='New Note')
+        self.my_btn.Bind(wx.EVT_BUTTON, self.on_press)
+        my_sizer.Add(self.my_btn, 0, wx.ALL, 5)
+        panel.SetSizer(my_sizer)
+        self.Show()
+
+    def on_press(self, event):
+        print("Button Pressed")
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
